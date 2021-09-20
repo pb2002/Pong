988d208e8a1ff7af9f88d3a1568ab9173edb47d8
pb2002 <bakker.pepijn@gmail.com>
2021-09-21 01:38:55 +0200

    - Added Collision.cs

---
+++ Pong/Collision.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Pong
+{
+    public class Collision
+    {
+        public Entity other;
+        public Vector2 hit;
+        public Vector2 intersection;
+        
+    }
+}
